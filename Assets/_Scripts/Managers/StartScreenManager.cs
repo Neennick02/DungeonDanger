a2621bf13bbcbbf2b7c72af5ddce6c3be4839b1b
Nee_nnick <Yannickgarbe02@gmail.com>
2026-03-27 18:47:47 +0100

    feat: working on UI sounds

--- Assets/_Scripts/Managers/StartScreenManager.cs
+++ Assets/_Scripts/Managers/StartScreenManager.cs
@@ -1,9 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartScreenManager : MonoBehaviour
 {
     [SerializeField] private GameObject continueButton;
+    public List<AudioClip> UI_Accept = new List<AudioClip>();
+    public List<AudioClip> UI_Return = new List<AudioClip>();
 
     private void Awake()
     {   //only enable continue button if data is saved
@@ -23,11 +27,27 @@ public class StartScreenManager : MonoBehaviour
 
     public void LoadMainScene()
     {
-        SceneManager.LoadScene("LevelBuildingScene");
+        StartCoroutine(ClickRoutine(UI_Accept, "LevelBuildingScene"));
     }
     public void Quit()
     {
-        Application.Quit();
+        StartCoroutine(ClickRoutine(UI_Return));
+    }
+
+    private IEnumerator ClickRoutine(List<AudioClip> clips, string sceneName = default)
+    {
+        AudioManager.Instance.PlayClip(clips);
+        yield return new WaitForSeconds(0.3f);
+
+        
+        if(sceneName != null)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else //if there is no string than quit
+        {
+            Application.Quit();
+        }
     }
 
 }
