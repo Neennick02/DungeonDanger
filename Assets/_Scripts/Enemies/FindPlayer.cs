a66cc1d50c9a42c9037d9f7253bf28b1fa1fc0f4
Nee_nnick <Yannickgarbe02@gmail.com>
2026-04-06 23:49:28 +0100

    feat: music fades in during combat

--- Assets/_Scripts/Enemies/FindPlayer.cs
+++ Assets/_Scripts/Enemies/FindPlayer.cs
@@ -6,7 +6,26 @@ using UnityEngine;
 public class FindPlayer : MonoBehaviour
 {
     [SerializeField] private List<GameObject> enemies = new List<GameObject>();
+    private MusicManager musicManager;
     private bool asssigned = false;
+    private bool listEmpty;
+
+    private void Awake()
+    {
+        musicManager = FindFirstObjectByType<MusicManager>(); 
+    }
+    private void Update()
+    {
+        //remove empty from list
+        enemies.RemoveAll(enemy =>  enemy == null);
+
+        if (enemies.Count < 1 && !listEmpty)
+        {
+            listEmpty = true;
+            musicManager.ToggleCombatMusic(false);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         //if already assigned return
@@ -15,6 +34,8 @@ public class FindPlayer : MonoBehaviour
         //check for player
         if (other.gameObject.CompareTag("Player"))
         {
+            musicManager.ToggleCombatMusic(true);
+
             for (int i = 0; i < enemies.Count; i++)
             {
                 BaseEnemy script = enemies[i].GetComponent<BaseEnemy>();
--- Assets/_Scripts/Sound/MusicManager.cs
+++ Assets/_Scripts/Sound/MusicManager.cs
@@ -1,10 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
     [SerializeField] private AudioSource heartBeatSource;
+    [SerializeField] private AudioSource defaultMusicSource;
+    [SerializeField] private AudioSource combatMusicSource;
+    private float fadeOutDuration = 1f;
+    private float timer;
     public void ToggleHeartBeat(bool active)
     {
         heartBeatSource.enabled = active;
-    } 
+    }
+
+    public void ToggleCombatMusic(bool active)
+    {
+        if (active)
+        {
+            combatMusicSource.enabled = true;
+            combatMusicSource.time = 0;
+            StartCoroutine(FadeMusic(defaultMusicSource, combatMusicSource));
+        }
+        else
+        {
+            StartCoroutine(FadeMusic(combatMusicSource, defaultMusicSource));
+        }
+    }
+
+    private IEnumerator FadeMusic(AudioSource oldAudio, AudioSource newAudio)
+    {
+        timer = 0; 
+
+        while(timer < fadeOutDuration)
+        {
+            timer += Time.deltaTime;
+
+            oldAudio.volume = Mathf.Lerp(1, 0, timer / fadeOutDuration);
+            newAudio.volume = Mathf.Lerp(0, 1, timer / fadeOutDuration);
+
+            yield return null;
+        }
+    }
 }
