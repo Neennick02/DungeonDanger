db2dfa905f0207ae3b2a8f1014829a262218390f
Yannick Garbe <Yannickgarbe02@gmail.com>
2026-04-07 12:09:47 +0200

    add: sound to lift

--- Assets/_Scripts/PokeApi.cs
+++ Assets/_Scripts/PokeApi.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
 using TMPro;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Networking;
@@ -8,21 +11,22 @@ using UnityEngine.UI;
 public class PokeApi : MonoBehaviour
 {
     public TMP_InputField inputField;
-    public Image sprite;
-
+    public Image Preview;
+    public AudioSource Source;
     private void Update()
     {
         if (Keyboard.current.enterKey.wasPressedThisFrame)
         {
-            if(inputField.text.Length > 0)
-            {
+            
                 StartCoroutine(CheckApi());
-            }
+            
         }
     }
 
     private IEnumerator CheckApi()
     {
+        PokeData data;
+
         //zoekt de api op
         using (UnityWebRequest request = UnityWebRequest.Get("https://pokeapi.co/api/v2/pokemon/" + inputField.text))
         {
@@ -35,15 +39,57 @@ public class PokeApi : MonoBehaviour
                 yield break;
             }
 
-            PokeData data;
             data = JsonUtility.FromJson<PokeData>(request.downloadHandler.text);
-            Debug.Log(data.name);
+            Debug.Log(data.id);
+        }
+
+        if(data != null)
+        {
+            using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(data.sprites.front_default))
+            {
+                yield return request.SendWebRequest();
+                if (request.result != UnityWebRequest.Result.Success)
+                {
+                    yield break;
+                }
+                Texture2D preview = DownloadHandlerTexture.GetContent(request);
+                Preview.sprite = Sprite.Create(preview, new Rect(0, 0, preview.width, preview.height), new Vector2(0.5f, 0.5f));
+            }
+
+
+            //audio
+            using (UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(data.cries.legacy, AudioType.OGGVORBIS))
+            {
+                yield return request.SendWebRequest();
+                if(request.result != UnityWebRequest.Result.Success)
+                {
+                    yield break;
+                }
+                AudioClip clip = DownloadHandlerAudioClip.GetContent(request);
+                Source.clip = clip;
+                Source.Play();
+            }
         }
     }
+
+    [Serializable]
     public class PokeData
     {
-        public string name;
-        public Image sprite;
-
+        public string id;
+        public PokeSprite sprites;
+        public PokeCry cries;
+    }
+    [Serializable]
+    public class PokeSprite
+    {
+        public string front_default;
+        public string back_default;
+        public string front_shiny;
+        public string back_shiny;
+    }
+    [Serializable]
+    public class PokeCry
+    {
+        public string legacy;
     }
 }
